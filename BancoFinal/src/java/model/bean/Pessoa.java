344efb59597e7d5a046d4ac6958bd9eaa083239a
Home <Home@DESKTOP-FPHTC2G>
2018-10-14 12:34:42 -0300

    Amém irmão!

---
+++ BancoFinal/src/java/model/bean/Pessoa.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Home
+ */
+public class Pessoa {
+    
+    private int IdDaConta;
+    private int CPF;
+
+    public int getIdDaConta() {
+        return IdDaConta;
+    }
+
+    public void setIdDaConta(int IdDaConta) {
+        this.IdDaConta = IdDaConta;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+    
+}
